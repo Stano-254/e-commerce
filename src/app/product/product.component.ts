b6fe9bd03b1e55ae3df77a1892e9da9ef64dc951
stanley <stanley.alucheri@spinmobile.co.ke>
2023-04-07 16:43:43 +0300

    almost there

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
+  products = [
+    {"desc":"Essentials Men's Short-Sleeve Crewneck T-Shirt","image":"assets/img/shirt_image.png","price":12.00,"rating":4.9, 'reviews':2356},
+    {"desc":"Essentials Men's Short-Sleeve Crewneck T-Shirt","image":"assets/img/black_brown_shirt.png","price":18.00,"rating":4.9, 'reviews':2356},
+    {"desc":"Essentials Men's Short-Sleeve Crewneck T-Shirt","image":"assets/img/material_shirt.jpg","price":32.00,"rating":4.9, 'reviews':2356},
+    {"desc":"Essentials Men's Short-Sleeve Crewneck T-Shirt","image":"assets/img/jungle_green_stripes_shirt.jpg","price":22.00,"rating":4.9, 'reviews':2356},
+    {"desc":"Essentials Men's Short-Sleeve Crewneck T-Shirt","image":"assets/img/black_brown_shirt.png","price":18.00,"rating":4.9, 'reviews':2356},
+    {"desc":"Essentials Men's Short-Sleeve Crewneck T-Shirt","image":"assets/img/shirt_image.png","price":12.00,"rating":4.9, 'reviews':2356},
+  ]
+
+
 }
--- src/app/productdetails/productdetails.component.ts
+++ src/app/productdetails/productdetails.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./productdetails.component.scss']
 })
 export class ProductdetailsComponent {
+   view = "show"
+   no_view = ""
+
+  constructor(){
+
+  }
+  toggle_view(){
+    if(this.view==''){
+      this.view="show"
+      this.no_view=""
+    }
+    else{
+      this.view = ''
+      this.no_view="show"
+    }
+
+  }
 
 }
